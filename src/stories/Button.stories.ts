ac997db2ae5e9b0b5d153e61aa26d9ffe3c1c11c
Lanng21 <mailanbiun@gmail.com>
2024-03-04 19:54:03 +0700

    feat: implement button

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,20 +1,27 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from './Button';
+import { Button } from "@/components/Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    variant: {
+      options: ["outlined", "text", "contained"],
+      control: { type: "radio" },
+    },
+    buttonType: {
+      options: ["primary", "warning", "success", "error", "info"],
+      control: { type: "radio" },
+    },
   },
 } satisfies Meta<typeof Button>;
 
@@ -24,27 +31,21 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
+    variant: "contained",
+    text: "Button",
   },
 };
 
 export const Large: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: "large",
+    text: "Button",
   },
 };
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: "small",
+    text: "Button",
   },
 };
